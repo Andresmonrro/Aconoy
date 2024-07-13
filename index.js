a66d053a3c61b6056f699e48118f1bed851ee2c8
andreMMonrro <monroymarqueza@gmail.com>
2024-07-13 02:03:22 -0500

    x

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-const { Client, Events } = require("discord.js");
+const { Client, Events, channelMention, channelLink, EmbedBuilder } = require("discord.js");
 const config = require("./config.json");
+const Discord = require("discord.js")
 
 const prefix = "!u";
+const pruebasChannelId = "1261563562342748160";
 
 const client = new Client({
     intents: 53608447,
 });
 
 client.on(Events.ClientReady, async () => {
-  console.log(`conectado como ${client.user.username}`);
+    console.log(`conectado como ${client.user.username}`);
 });
 
 client.on(Events.MessageCreate, async (message) => {
-    if((message.content === 'hola' || message.content === 'Hola') &&  message.author.bot) {
-        message.reply("hola, eres un bot")
-    } else if (message.content === 'hola' || message.content === 'Hola') {
-        message.reply("hola bro")
-    }
+  const embed = {
+    color: 0x0099ff,
+	  title: 'metenme',
+    description: `${message}`
+  }
+  /* const pruebasChannel =  client.channels.fetch(pruebasChannelId);
+  const chnl = `${message} aaaaa`
+  console.log(chnl);
+  pruebasChannel.send(embed); */
+  if (
+    (message.content === "asd" || message.content === "Asd") &&
+    message.author.bot
+  ) {
+    message.reply({ embeds: [embed]});
+  } else if (message.content === "asd" || message.content === "Asd") {
+    message.reply({ embeds: [embed]});
+  }
 });
+
+/* client.on('channelCreate', (channelId)=>{
+  const pruebasChannel =  client.channels.fetch(pruebasChannelId);
+  const chnl = channelId.guildId
+  console.log(chnl);
+  pruebasChannel.send(chnl);
+});
+
+client.on(Events.ChannelDelete, async (channelId) => {
+  const pruebasChannel = await client.channels.fetch(pruebasChannelId);
+  const chnl = channelId.guildId
+  console.log(chnl);
+  const embed = new Discord.createComponent()
+      .setTitle(' título')
+      .setDescription(`pruebaaaaa ${channelId}`)
+      .setColor('#00ff00');
+
+  pruebasChannel.send(embed);
+}); */
 
 client.login(config.BOT_TOKEN);
